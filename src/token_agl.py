f42a8ccec646e0e3f8d3d7b39eb1e43ee90ccdbe
JettNehls <jettnehls1@gmail.com>
2025-09-15 14:06:57 -0500

    Added is commands to token

--- src/token_agl.py
+++ src/token_agl.py
@@ -17,9 +17,53 @@ class Token:
         self.token_type: TokenType = token_type
         self.token_value: int | None = token_value
 
-    # Add "is" commands
+    def isTerminator(self):
+        return self.token_type == TokenType.terminator
+
+    def isInteger(self):
+        return self.token_type == TokenType.int_token
+
+    def isIdentifier(self):
+        return self.token_type == TokenType.identifier
+
+    def isKeyword(self):
+        return self.token_type == TokenType.keyword
+
+    def isBoolean(self):
+        return self.token_type == TokenType.boolean
+
+    def isPrimitiveIdentifier(self):
+        return self.token_type == TokenType.primitive_identifier
+
+    def isPunctuation(self):
+        return self.token_type == TokenType.punctuation
+
+    def isOperator(self):
+        return self.token_type == TokenType.operator
+
+    def isEOF(self):
+        return self.token_type == TokenType.eof
 
     def value(self):
         return self.token_value
 
-    # Add repr command
+    def __repr__(self):
+        if self.isTerminator():
+            return 'terminator'
+        elif self.isInteger():
+            return f'integer = {self.token_value}'
+        elif self.isIdentifier():
+            return f'identifier = {self.token_value}'
+        elif self.isKeyword():
+            return f'keyword = {self.token_value}'
+        elif self.isBoolean():
+            return f'boolean = {self.token_value}'
+        elif self.isPrimitiveIdentifier():
+            return f'primitive = {self.token_value}'
+        elif self.isPunctuation():
+            return f'punctuation = {self.token_value}'
+        elif self.isOperator():
+            return f'operator = {self.token_value}'
+        else:  
+            return 'EOF'
+
