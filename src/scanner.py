c069164257640a0c7cdf6bf275b5b26c599632ed
GGonnerman <gaston95g@gmail.com>
2025-09-16 09:51:44 -0500

    Implemented basic scanner functionality

--- src/scanner.py
+++ src/scanner.py
@@ -1,27 +1,13 @@
-from enum import Enum
-from typing import TYPE_CHECKING
-
 from token_agl import Token, TokenType
 
-if TYPE_CHECKING:
-    from collections.abc import Iterator
-
-
-class State(Enum):
-    looking = 1
-    integer = 2
-    string = 3
-
-
-LOWERCASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz"
-UPPERCASE_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-ALPHABET = LOWERCASE_ALPHABET + UPPERCASE_ALPHABET
+ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 NON_ZERO_INTEGERS = "123456789"
 INTEGERS = "0" + NON_ZERO_INTEGERS
 IDENTIFIER_CHARACTERS = ALPHABET + INTEGERS + "_"
 PUNCTUATION = "(),:"
-OPERATORS = "*-/<="
-SKIPPABLE = "\t\n "
+OPERATORS = "+-*/<="
+SKIPPABLE = "\t\n ("
+DELIMITERS = PUNCTUATION + OPERATORS + SKIPPABLE + "_"
 KEYWORDS = [
     "integer",
     "string",
@@ -39,92 +25,158 @@ BOOLEAN_LITERALS = ["true", "false"]
 
 
 class Scanner:
-    """One item buffer for reading an item-position sequence"""
-
     def __init__(self, program: str):
-        self.iter: Iterator[str] = iter(program)
-        self.pos = None
-        self.done = False
-        self.value = ""
-        self.position = 0
-        self._advance()
+        self.program: str = program
+        self.has_terminated: bool = False
+        self.position: int = 0
+        self.working_position: int = 0
+        self.accum: str = ""
 
-    def _advance(self):
-        try:
-            self.item, self.pos = next(self.iter)
-        except StopIteration:
-            self.done = True
+    def next(self):
+        return self._next(update_position=True)
 
     def peek(self):
-        """return current item"""
+        return self._next(update_position=False)
 
-        if self.done:
-            # raise exception
-            raise Exception("Cannot peek terminated scanner")
+    def _next(self, *, update_position: bool = True):
+        if self.has_terminated:
+            raise Exception("Cannot get more tokens after scanner has terminated")
 
-        return self.item
+        token: Token | None = None
+        while token is None:
+            self.position = self.working_position
+            token = self._stage0()
 
-    def get(self):
-        """return current item and advance to next"""
-
-        assert not self.done
-        item = self.item
-        self._advance()
-        return item
-
-    def scan_token(self):
-        # need checks for all token types
-        # skip white space and comments
-        # check for end of file
-        pass
-
-    def _state0(self, word: str, pos: int):
-        if pos >= len(word):
-            print("reject")
+        if update_position:
+            self.position = self.working_position
+            self.has_terminated = token.is_eof()
         else:
-            current_char = word[pos]
-            if current_char == "0":
-                print("reject")
-            self.value = ""
-            if current_char in ALPHABET:
-                self._state1(word, pos + 1)
-            elif current_char in NON_ZERO_INTEGERS:
-                self._state2(word, pos + 1)
-            elif current_char == "(":
-                # here we go to a combined "punctuation or whitespace" field
-                # which disambiguates
-                self._state3(word, pos + 1)
-            elif current_char in PUNCTUATION:
-                self._state4(word, pos + 1)
-            elif current_char in OPERATORS:
-                self._state5(word, pos + 1)
-            elif current_char in SKIPPABLE:
-                self._state6(word, pos + 1)
-            else:
-                raise Exception("Invalid character")
+            self.working_position = self.position
 
-    def _state1(self, word: str, pos: int):
-        if pos >= len(word):
-            print("reject")
-        else:
-            current_char = word[pos]
-            self.value += current_char
-            if current_char in IDENTIFIER_CHARACTERS:
-                return self._state1(word, pos + 1)
-            return Token(TokenType.identifier, self.value)
-        return None
+        return token
 
-    def _state2(self, word: str, pos: int):
-        pass
+    def _stage0(self):
+        if self.working_position >= len(self.program):
+            return Token(TokenType.eof)
+        self.accum = ""
+        char = self.program[self.working_position]
+        if char in ALPHABET:
+            self.accum += char
+            self.working_position += 1
+            return self._stage1()
+        if char in "0":
+            self.accum += char
+            self.working_position += 1
+            return self._stage2()
+        if char in NON_ZERO_INTEGERS:
+            self.accum += char
+            self.working_position += 1
+            return self._stage3()
+        if char in OPERATORS:
+            self.accum += char
+            self.working_position += 1
+            return self._stage4()
+        if char in "(":
+            self.accum += char
+            self.working_position += 1
+            return self._stage5()
+        if char in SKIPPABLE:
+            self.accum += char
+            self.working_position += 1
+            return self._stage9()
+        if char in PUNCTUATION:
+            self.accum += char
+            self.working_position += 1
+            return self._stage10()
+        raise Exception(f'Unknown char: "{char}"')
 
-    def _state3(self, word: str, pos: int):
-        pass
+    def _categorize_identifier(self, identifier) -> Token:
+        if identifier in BOOLEAN_LITERALS:
+            return Token(TokenType.boolean, self.accum)
+        if identifier in PRIMATIVE_IDENTIFIERS:
+            return Token(TokenType.primitive_identifier, self.accum)
+        if identifier in KEYWORDS:
+            return Token(TokenType.keyword, self.accum)
+        return Token(TokenType.identifier, self.accum)
 
-    def _state4(self, word: str, pos: int):
-        pass
+    def _stage1(self) -> Token:
+        if self.working_position >= len(self.program):
+            return self._categorize_identifier(self.accum)
+        char = self.program[self.working_position]
+        if char in IDENTIFIER_CHARACTERS:
+            self.accum += char
+            self.working_position += 1
+            return self._stage1()
+        if char in DELIMITERS:
+            return self._categorize_identifier(self.accum)
+        raise Exception(f"Invalid char in identifier '{char}'")
 
-    def _state5(self, word: str, pos: int):
-        pass
+    def _stage2(self) -> Token:
+        if self.working_position >= len(self.program):
+            return Token(TokenType.int_token, self.accum)
+        char = self.program[self.working_position]
+        if char in DELIMITERS:
+            return Token(TokenType.int_token, self.accum)
+        if char in INTEGERS:
+            raise Exception("Integer cannot start with leading 0")
+        raise Exception("Invalid char in integer")
 
-    def _state6(self, word: str, pos: int):
-        pass
+    def _stage3(self) -> Token:
+        if self.working_position >= len(self.program):
+            return Token(TokenType.int_token, self.accum)
+        char = self.program[self.working_position]
+        if char in INTEGERS:
+            self.accum += char
+            self.working_position += 1
+            return self._stage3()
+        if char in DELIMITERS:
+            return Token(TokenType.int_token, self.accum)
+        raise Exception("Invalid char in integer")
+
+    def _stage4(self) -> Token:
+        return Token(TokenType.operator, self.accum)
+
+    def _stage5(self) -> Token | None:
+        if self.working_position >= len(self.program):
+            return Token(TokenType.punctuation, self.accum)
+        char = self.program[self.working_position]
+        if char in "*":
+            self.accum += char
+            self.working_position += 1
+            return self._stage6()
+        return Token(TokenType.punctuation, self.accum)
+
+    def _stage6(self) -> Token | None:
+        if self.working_position >= len(self.program):
+            raise Exception("All comments must be terminated before program ends")
+        char = self.program[self.working_position]
+        self.accum += char
+        self.working_position += 1
+        if char in "*":
+            return self._stage7()
+        return self._stage6()
+
+    def _stage7(self) -> Token | None:
+        if self.working_position >= len(self.program):
+            raise Exception("All comments must be terminated before program ends")
+        char = self.program[self.working_position]
+        self.accum += char
+        self.working_position += 1
+        if char in ")":
+            return self._stage8()
+        return self._stage6()
+
+    def _stage8(self) -> None:
+        return
+
+    def _stage9(self) -> None:
+        return
+        # Don't return anything; this is whitespace which is ignored
+        # In theory could return token with type whitespace, but I don't
+        # think whitespace is supposed to be a token.
+
+    def _stage10(self) -> Token:
+        return Token(TokenType.punctuation, self.accum)
+
+    def has_next(self) -> bool:
+        return not self.has_terminated
