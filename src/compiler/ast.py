254f8e03aa67c6a9d244297f6b1d5fef7789a5a7
GGonnerman <gaston95g@gmail.com>
2025-10-15 00:45:55 -0500

    WIP: Printing ast and converting to dot

--- src/compiler/ast.py
+++ src/compiler/ast.py
@@ -87,7 +87,7 @@ class ASTNode(ABC):  # noqa: B024
         return token
 
     def __str__(self) -> str:
-        return f"<{self.__class__.__name__} />"
+        return self.__class__.__name__
 
     def get_token_value(self, token: Token) -> str:
         value = token.value()
@@ -165,72 +165,81 @@ class IntegerType(Type):
     def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
         pass
 
+    def __str__(self):
+        return "Integer"
+
 
 class BooleanType(Type):
     def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
         pass
+
+    def __str__(self):
+        return "Boolean"
 
 
 class Expression(ASTNode, ABC):
     pass
 
 
-class EqualsExpression(Expression):
-    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
-        self.right_side: Expression = self.validate(semantic_stack.pop(), Expression)
-        self.left_side: Expression = self.validate(semantic_stack.pop(), Expression)
-
-
-class LessThanExpression(Expression):
-    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
-        self.right_side: Expression = self.validate(semantic_stack.pop(), Expression)
-        self.left_side: Expression = self.validate(semantic_stack.pop(), Expression)
-
-
-class OrExpression(Expression):
-    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
-        self.right_side: Expression = self.validate(semantic_stack.pop(), Expression)
-        self.left_side: Expression = self.validate(semantic_stack.pop(), Expression)
-
-
-class PlusExpression(Expression):
-    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
-        self.right_side: Expression = self.validate(semantic_stack.pop(), Expression)
-        self.left_side: Expression = self.validate(semantic_stack.pop(), Expression)
-
-
-class MinusExpression(Expression):
-    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
-        self.right_side: Expression = self.validate(semantic_stack.pop(), Expression)
-        self.left_side: Expression = self.validate(semantic_stack.pop(), Expression)
-
-
-class TimesExpression(Expression):
-    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
-        self.right_side: Expression = self.validate(semantic_stack.pop(), Expression)
-        self.left_side: Expression = self.validate(semantic_stack.pop(), Expression)
-
-
-class DivideExpression(Expression):
-    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
-        self.right_side: Expression = self.validate(semantic_stack.pop(), Expression)
-        self.left_side: Expression = self.validate(semantic_stack.pop(), Expression)
-
-
-class AndExpression(Expression):
-    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
-        self.right_side: Expression = self.validate(semantic_stack.pop(), Expression)
-        self.left_side: Expression = self.validate(semantic_stack.pop(), Expression)
-
-
-class NotExpression(Expression):
+class UnaryExpression(Expression, ABC):
     def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
         self.value: Expression = self.validate(semantic_stack.pop(), Expression)
 
 
-class UnaryMinusExpression(Expression):
+class BinaryExpression(Expression, ABC):
     def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
-        self.value: Expression = self.validate(semantic_stack.pop(), Expression)
+        self.right_side: Expression = self.validate(semantic_stack.pop(), Expression)
+        self.left_side: Expression = self.validate(semantic_stack.pop(), Expression)
+
+
+class EqualsExpression(BinaryExpression):
+    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
+        super().__init__(semantic_stack, most_recent_token)
+
+
+class LessThanExpression(BinaryExpression):
+    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
+        super().__init__(semantic_stack, most_recent_token)
+
+
+class OrExpression(BinaryExpression):
+    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
+        super().__init__(semantic_stack, most_recent_token)
+
+
+class PlusExpression(BinaryExpression):
+    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
+        super().__init__(semantic_stack, most_recent_token)
+
+
+class MinusExpression(BinaryExpression):
+    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
+        super().__init__(semantic_stack, most_recent_token)
+
+
+class TimesExpression(BinaryExpression):
+    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
+        super().__init__(semantic_stack, most_recent_token)
+
+
+class DivideExpression(BinaryExpression):
+    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
+        super().__init__(semantic_stack, most_recent_token)
+
+
+class AndExpression(BinaryExpression):
+    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
+        super().__init__(semantic_stack, most_recent_token)
+
+
+class NotExpression(UnaryExpression):
+    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
+        super().__init__(semantic_stack, most_recent_token)
+
+
+class UnaryMinusExpression(UnaryExpression):
+    def __init__(self, semantic_stack: SemanticStack, most_recent_token: Token | None):
+        super().__init__(semantic_stack, most_recent_token)
 
 
 class IfExpression(Expression):
@@ -290,6 +299,9 @@ class Identifier(Expression):
         else:
             self.value = self.get_token_value(self._token)
 
+    def __str__(self):
+        return f"{self.value}"
+
 
 action_to_astnode: dict[
     SemanticAction,
@@ -321,3 +333,139 @@ action_to_astnode: dict[
     SemanticAction.MAKE_INTEGER_LITERAL: IntegerLiteral,
     SemanticAction.MAKE_BOOLEAN_LITERAL: BooleanLiteral,
 }
+
+
+def display_astnode(node: ASTNode, indent: int = 0, spacer: str = "  "):
+    main_indent: str = indent * spacer
+    sub_indent: str = (indent + 1) * spacer
+    if isinstance(node, Program):
+        print(f"{main_indent}{node}")
+        display_astnode(node.definition_list, indent + 1, spacer)
+    elif isinstance(node, DefinitionList):
+        for definition in node.definitions:
+            display_astnode(definition, indent, spacer)
+    elif isinstance(node, Definition):
+        print(f"{main_indent}{node}")
+        print(f"{sub_indent}name {node.name}")
+        print(f"{sub_indent}parameters")
+        display_astnode(node.parameters, indent + 2, spacer)
+        print(f"{sub_indent}returns {node.return_type}")
+        print(f"{sub_indent}body")
+        display_astnode(node.body, indent + 2, spacer)
+    elif isinstance(node, ParameterList):
+        for parameter in node.definitions:
+            display_astnode(parameter, indent, spacer)
+    elif isinstance(node, IdWithType):
+        print(f"{main_indent}{node.type} {node.name}")
+    elif isinstance(node, Body):
+        for print_stm in node.print_expressions:
+            display_astnode(print_stm, indent, spacer)
+        display_astnode(node.body, indent, spacer)
+    elif isinstance(node, FunctionCallExpression):
+        print(f"{main_indent}function call")
+        print(f"{sub_indent}name {node.function_name}")
+        display_astnode(node.argument_list, indent + 1, spacer)
+    elif isinstance(node, ArgumentList):
+        if len(node.arguments) == 0:
+            return
+        print(f"{main_indent}arguments")
+        for argument in node.arguments:
+            display_astnode(argument, indent + 1, spacer)
+    elif isinstance(node, Argument):
+        display_astnode(node.value, indent, spacer)
+    elif isinstance(node, BinaryExpression):
+        print(f"{main_indent}{node}")
+        if isinstance(node.left_side, (Identifier, Literal)):
+            print(f"{sub_indent}left_side {node.left_side}")
+        else:
+            print(f"{sub_indent}left_side")
+            display_astnode(node.left_side, indent + 2, spacer)
+        if isinstance(node.right_side, (Identifier, Literal)):
+            print(f"{sub_indent}right_side {node.right_side}")
+        else:
+            print(f"{sub_indent}right_side")
+            display_astnode(node.right_side, indent + 2, spacer)
+    elif isinstance(node, UnaryExpression):
+        print(f"{main_indent}{node}")
+        if isinstance(node.value, (Identifier, Literal)):
+            print(f"{sub_indent}value {node.value}")
+        else:
+            print(f"{sub_indent}value")
+            display_astnode(node.value, indent + 2, spacer)
+    elif isinstance(node, (IntegerLiteral, BooleanLiteral)):
+        print(f"{main_indent}{node} {node.value}")
+    elif isinstance(node, Identifier):
+        print(f"{main_indent}{node.value}")
+    else:
+        raise NotImplementedError(f"Node of type {node} has not yet been implemented")
+
+
+def astnode_to_dot(node: ASTNode):
+    print("digraph ast {")
+    _astnode_to_dot(node)
+    print("}")
+
+
+def link(source: ASTNode, destination: ASTNode, label: str | None = None):
+    if label:
+        print(f'{hash(source)} -> {hash(destination)} [label = "{label}"]')
+    else:
+        print(f"{hash(source)} -> {hash(destination)}")
+    _astnode_to_dot(destination)
+
+
+def _astnode_to_dot(node: ASTNode):
+    key = hash(node)
+    if isinstance(node, Program):
+        print(f'{key} [label = "{node}"]')
+        link(node, node.definition_list)
+    elif isinstance(node, DefinitionList):
+        print(f'{key} [label = "{node}"]')
+        for definition in node.definitions:
+            link(node, definition)
+    elif isinstance(node, Definition):
+        print(
+            f'{key} [label = "{node}\nname: {node.name}\nreturns {node.return_type}"]',
+        )
+        # link(node, node.name, "name")
+
+        link(node, node.parameters, "parameters")
+        link(node, node.body, "body")
+    elif isinstance(node, ParameterList):
+        print(f'{key} [label = "{node}"]')
+        for parameter in node.definitions:
+            link(node, parameter)
+    elif isinstance(node, IdWithType):
+        print(f'{key} [label = "name {node.name}\ntype {node.type}"]')
+    elif isinstance(node, Body):
+        print(f'{key} [label = "{node}"]')
+        for print_expression in node.print_expressions:
+            link(node, print_expression)
+        link(node, node.body)
+    elif isinstance(node, BinaryExpression):
+        print(f'{key} [label = "{node}"]')
+        link(node, node.left_side, "left_side")
+        link(node, node.right_side, "right_side")
+    elif isinstance(node, UnaryExpression):
+        print(f'{key} [label = "{node}"]')
+        link(node, node.value)
+    elif isinstance(node, FunctionCallExpression):
+        print(f'{key} [label = "{node}\n{node.function_name}"]')
+        link(node, node.argument_list)
+    elif isinstance(node, ArgumentList):
+        print(f'{key} [label = "{node}"]')
+        for argument in node.arguments:
+            link(node, argument)
+    elif isinstance(node, Argument):
+        print(f'{key} [label = "{node}"]')
+        link(node, node.value)
+    elif isinstance(node, IfExpression):
+        print(f'{key} [label = "{node}"]')
+        link(node, node.condition, "condition")
+        link(node, node.consequent, "consequent")
+        link(node, node.alternative, "alternative")
+    elif isinstance(node, (Identifier, IntegerLiteral, BooleanLiteral)):
+        # TODO: Check if this seems right
+        print(f'{key} [label = "{node}"]')
+    else:
+        return
