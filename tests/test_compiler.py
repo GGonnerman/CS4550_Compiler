59f3f6033fca99bd65266bd4fe28ec2efeff7873
GGonnerman <gaston95g@gmail.com>
2025-12-05 15:11:59 -0600

    Added many tests for the compile

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -2,14 +2,10 @@ import subprocess
 from dataclasses import dataclass
 from pathlib import Path
 
+import pytest
+
 base_path = Path(__file__).parent
 tm_cli_path = base_path / "tm-cli-go"
-programs_path = base_path / "programs"
-
-# *   $ klein divide 7 12 2                                         *
-# *   5                                                             *
-# *   8                                                             *
-# *   4                                                             *
 
 
 @dataclass
@@ -19,9 +15,35 @@ class FileTestParams:
     output: list[str]
 
 
-def run_file(obj: FileTestParams):
-    file_path = programs_path / obj.name
+def create_and_run_program(
+    contents: str,
+    arguments: list[str],
+    output: list[str],
+):
+    temp_filename = "temp"
+    with open(base_path / f"{temp_filename}.kln", "w+") as outfile:
+        _ = outfile.write(contents)
+    obj = FileTestParams(
+        temp_filename,
+        arguments,
+        output,
+    )
+    run_file(obj, base_path)
+
+
+def run_program(obj: FileTestParams):
+    programs_path = base_path / "programs"
+    run_file(obj, programs_path)
+
+
+def run_file(obj: FileTestParams, program_path: Path):
+    file_path = program_path / obj.name
     kleinc_path = base_path / ".." / "kleinc"
+
+    generated_tm_file = program_path / f"{obj.name}.tm"
+
+    # We never want to be testing 'stale' tm files if things fail to build
+    generated_tm_file.unlink(missing_ok=True)
 
     res = subprocess.run(
         [kleinc_path, file_path],
@@ -30,6 +52,9 @@ def run_file(obj: FileTestParams):
     ).stdout.decode(
         "utf-8",
     )
+
+    if res:
+        raise Exception(f"Failed to compile klein program\n{res}")
 
     res = subprocess.run(
         [tm_cli_path, file_path, *obj.arguments],
@@ -48,15 +73,25 @@ def run_file(obj: FileTestParams):
     )
 
 
+def test_print_one():
+    run_program(
+        FileTestParams(
+            "print-one",
+            [],
+            ["1", "1"],
+        ),
+    )
+
+
 def test_divide():
-    run_file(
+    run_program(
         FileTestParams(
             "divide",
             ["7", "12", "2"],
             ["5", "8", "4"],
         ),
     )
-    run_file(
+    run_program(
         FileTestParams(
             "divide",
             ["7", "12", "4"],
@@ -66,7 +101,7 @@ def test_divide():
 
 
 def test_sum_factors():
-    run_file(
+    run_program(
         FileTestParams(
             "sum-factors",
             ["38"],
@@ -75,11 +110,263 @@ def test_sum_factors():
     )
 
 
+def test_is_excellent():
+    run_program(
+        FileTestParams(
+            "is-excellent",
+            [
+                "140400",
+            ],
+            ["1"],
+        ),
+    )
+
+
+@pytest.mark.xfail
+def test_generate_excellent():
+    run_program(
+        FileTestParams(
+            "generate-excellent",
+            ["6"],
+            [
+                "140400",
+                "190476",
+                "216513",
+                "300625",
+                "334668",
+                "416768",
+                "484848",
+                "530901",
+            ],
+        ),
+    )
+
+
 def test_sieve():
-    run_file(
+    run_program(
         FileTestParams(
             "sieve",
             ["7"],
             ["2", "3", "0", "5", "0", "7", "1"],
         ),
+    )
+
+
+def test_farey():
+    run_program(
+        FileTestParams(
+            "farey",
+            ["127", "1000", "74"],
+            ["8", "63"],
+        ),
+    )
+    run_program(
+        FileTestParams(
+            "farey",
+            ["367879", "1000000", "100"],
+            ["32", "87"],
+        ),
+    )
+
+
+def test_is_special():
+    run_program(
+        FileTestParams(
+            "is-special",
+            ["16"],
+            ["1"],
+        ),
+    )
+    run_program(
+        FileTestParams(
+            "is-special",
+            ["17"],
+            ["0"],
+        ),
+    )
+
+
+def test_is_cantor():
+    for cantor_name in [
+        "is-cantor-number-bool",
+        "is-cantor-number-fast",
+        "is-cantor-number-v4",
+        "is-cantor-number",
+    ]:
+        run_program(
+            FileTestParams(
+                cantor_name,
+                ["9081"],
+                ["1"],
+            ),
+        )
+        run_program(
+            FileTestParams(
+                cantor_name,
+                ["9083"],
+                ["0"],
+            ),
+        )
+
+
+def test_horner():
+    run_program(
+        FileTestParams(
+            "horner-hardcoded",
+            ["21"],
+            ["7548"],
+        ),
+    )
+
+
+def test_palindrome():
+    run_program(
+        FileTestParams(
+            "palindrome",
+            ["12344321"],
+            ["12344321", "12344321", "1"],
+        ),
+    )
+    run_program(
+        FileTestParams(
+            "palindrome",
+            ["1234321"],
+            ["1234321", "1234321", "1"],
+        ),
+    )
+    run_program(
+        FileTestParams(
+            "palindrome",
+            ["1234320"],
+            ["1234320", "234321", "0"],
+        ),
+    )
+
+
+# Is the sqrt function supposed to be this far off (due to integer rounding) or
+# is there something else wrong here?
+def test_sqrt_newton():
+    run_program(
+        FileTestParams(
+            "sqrt-newton",
+            ["100", "10000"],
+            ["26"],
+        ),
+    )
+
+
+def test_and_shortcircuit():
+    crash = """
+    function crash(): boolean
+        crash()
+    """
+    create_and_run_program(
+        """
+    function main(): boolean
+        false and crash()
+    """
+        + crash,
+        [],
+        ["0"],
+    )
+    create_and_run_program(
+        """
+    function main(): boolean
+        false and true
+    """,
+        [],
+        ["0"],
+    )
+    create_and_run_program(
+        """
+    function main(): boolean
+        true and false
+    """,
+        [],
+        ["0"],
+    )
+    create_and_run_program(
+        """
+    function main(): boolean
+        true and true
+    """,
+        [],
+        ["1"],
+    )
+
+
+def test_or_shortcircuit():
+    crash = """
+    function crash(): boolean
+        crash()
+    """
+    create_and_run_program(
+        """
+    function main(): boolean
+        false or false
+    """,
+        [],
+        ["0"],
+    )
+    create_and_run_program(
+        """
+    function main(): boolean
+        false or true
+    """,
+        [],
+        ["1"],
+    )
+    create_and_run_program(
+        """
+    function main(): boolean
+        true or crash()
+    """
+        + crash,
+        [],
+        ["1"],
+    )
+
+
+def test_arg_order():
+    create_and_run_program(
+        """
+    function main(a: integer, b: integer, c: integer): boolean
+        print(a)
+        print(b)
+        print(c)
+        false
+    """,
+        ["123", "123456789", "789"],
+        ["123", "123456789", "789", "0"],
+    )
+    create_and_run_program(
+        """
+    function main(a: integer, b: integer, c: integer): boolean
+        out(a, b, c)
+
+    function out(d: integer, e: integer, f: integer): boolean
+        print(d)
+        print(e)
+        print(f)
+        false
+    """,
+        ["123", "123456789", "789"],
+        ["123", "123456789", "789", "0"],
+    )
+
+
+def test_recursive():
+    create_and_run_program(
+        """
+    function main(max: integer): integer
+        sum_to(max)
+
+    function sum_to(max: integer): integer
+        if max < 1 then
+            max
+        else
+            max + sum_to(max - 1)
+    """,
+        ["10"],
+        ["55"],
     )
